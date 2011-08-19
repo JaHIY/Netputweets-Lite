bf9c8d5ad14a841ae630b36bf2e82da53c36502e
JaHIY <jaklsy@gmail.com>
2011-08-19 19:28:43 +0800

    update to Official Dabr r432

--- common/twitter.php
+++ common/twitter.php
@@ -685,8 +685,9 @@ function twitter_fetch($url) {
   global $services_time;
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
-  //curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-  $user_agent = "Mozilla/5.0 (compatible; dabr; " . BASE_URL . ")";
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+  //$user_agent = "Mozilla/5.0 (compatible; dabr; " . BASE_URL . ")";
+  $user_agent = "Mozilla/5.0 (compatible; Netputweets Lite!)";
   curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $fetch_start = microtime(1);
